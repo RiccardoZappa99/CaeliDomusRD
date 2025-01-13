df0f7358588d60bd8f44acf6ab2b492c541d570b
RiccardoZappa99 <151678003+RiccardoZappa99@users.noreply.github.com>
2025-01-13 20:17:26 +0100

    Implementazione Iconfiguration per gestione accesso a string connection

--- CaeliDomusRD/DB/DBContext.cs
+++ CaeliDomusRD/DB/DBContext.cs
@@ -1,25 +1,36 @@
 using FreeSql;
 using FreeSql.Extensions.EntityUtil;
 using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Configuration.Json;
+using System.ComponentModel;
 
 //uso IFreeSql che è l'oggetto di primo livello dell'ORMcui si hanno tutti gli
 //accessi alle relative funzioni di CRUD ed estensioni (nuget)
 namespace DBContext
 {
-internal class DB
-{
-private IFreeSql _db;  
-private IConfiguration _config;
-public IFreeSql Db { get => _db; set => _db = value; }  
-public IConfiguration Config { get => _config; set => _config = value; }
+    internal class DB : IDisposable
+    {
+        private IFreeSql _db;
+        private string _config;
+        public IFreeSql Db { get => _db; set => _db = value; }
+        public string Config { get => _config; set => _config = value; }
 
-        public DB () 
-{
-    Db = new FreeSql.FreeSqlBuilder()
-  .UseConnectionString(FreeSql.DataType.Sqlite, Config.GetConnectionString("CaeliDomusRD"))
-  .UseAutoSyncStructure(true) //automatically synchronize the entity structure to the database
-  .Build();
-}
+        public DB()
+        {
+            var configuration = new ConfigurationBuilder()
+                        .SetBasePath("C:\\Users\\AryRicky\\Documents\\GitHub\\CaeliDomusRD\\CaeliDomusRD")
+                        .AddJsonFile("appsettings.json")
+                        .Build(); 
+            Config = configuration.GetConnectionString("CaeliDomusRD");
+            Db = new FreeSql.FreeSqlBuilder()
+          .UseConnectionString(FreeSql.DataType.Sqlite, Config)
+          .UseAutoSyncStructure(true) //automatically synchronize the entity structure to the database
+          .Build();
+        }
 
+        public void Dispose()
+        {
+            Db.Dispose();   //si puo valutare di ampliare con argomento di controllo bool e logiche piuu approfondite
+        }
     }
 }
--- CaeliDomusRD/ThermistorReader/GpioReader.cs
+++ CaeliDomusRD/ThermistorReader/GpioReader.cs
@@ -1,6 +1,11 @@
+using CaeliDomusRD.DbModelEntities;
+using DBContext;
+using Microsoft.Extensions.Configuration;
+using Renci.SshNet;
 using System;
 using System.Device.Gpio;
 using System.Linq;
+using System.Net.Sockets;
 
 //            Vout= (R1/(R(T)+R1)*Vin
 //            R(T)=((Vin/Vout)-1)*R1
@@ -16,6 +21,7 @@ namespace CaeliDomusRD
         private static double _voltageOutput = 0;
         private static double _voltageInput = 0;
         private static double []_voltageSampling = {};
+        private IConfiguration _configuration;
 
         public static double VoltageOutput { get => _voltageOutput; set => _voltageOutput = value; }
         public static double VoltageInput { get => _voltageInput; set => _voltageInput = value; }
@@ -37,15 +43,30 @@ namespace CaeliDomusRD
             const double _beta= 3974.0;
             const double _typicalRoomTemperature = 298.15;
 
-            using (GpioReader _Pi5 = new GpioReader())
+            // PER USO IN AMBIENTE NATIVO PI5
+
+            //using (GpioReader _Pi5 = new GpioReader())
+            //{
+            //for(int i = 0; i>9 ; i++)
+            //{
+            //    VoltageSampling[i] = Convert.ToDouble(_Pi5.Read(PinNumber));
+            //    //campiono in piccoli frammenti di tempi diversi
+            //    Thread.Sleep(10); 
+            //}
+            //}
+
+            using (SshClient pi5 = new SshClient("192.168.1.61", "bombadil", "Bombadil25"))   // prendere dalla config
             {
-            for(int i = 0; i>9 ; i++)
-            {
-                VoltageSampling[i] = Convert.ToDouble(_Pi5.Read(PinNumber));
-                //campiono in piccoli frammenti di tempi diversi
-                Thread.Sleep(10); 
+                pi5.Connect();
+                for (int i = 0; i > 9; i++)
+                {
+                    VoltageSampling[i] = Convert.ToDouble(pi5.RunCommand("gpioget 0 26").Result); //Convert.ToDouble(_Pi5.Read(PinNumber));
+                    //campiono in piccoli frammenti di tempi diversi
+                    Thread.Sleep(10);
+                }
+                pi5.Disconnect();
             }
-            }
+
 
             // calcolo la media degli output rilevati. somma dei campioni diviso il numero totali di essi.
             VoltageOutput = VoltageSampling.Sum()/VoltageSampling.Count(); 
@@ -60,10 +81,20 @@ namespace CaeliDomusRD
            // converto in Celsius
            _CelsiusValue = _KelvinValue - 273.15;
 
-           //scrivo a db
-            
-            
+            //scrivo a db
 
+            using (DB _temperatureRD = new DB()) 
+            { 
+                Temperature temperature = new Temperature();
+
+                temperature.TemperatureValue = 1;
+                temperature.ReadTime = DateTime.Now;
+
+                _temperatureRD.Db.Insert<Temperature>(temperature);
+                
+            }
+
+            
         }
 
     }
